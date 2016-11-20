3e06201986f77f364e854edd7a0e76cd54bc9441
amandeepmittal <amandeepmittal@live.com>
2016-11-20 11:37:05 +0530

    inital commit ✨✨

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # amanhimself
 An npm package about myself.
+
+### Installation
+`$npm install -S amanhimself`
+
+### Usage
+```javascript
+const aman = require('amanhimself');
+
+console.log(aman.name.first);
+//=> 'aman'
+console.log(aman.projects.jade.description);
+//=> Jade/Pug snippets package for Atom.
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  name: {
+    first: "aman",
+    last: "mittal",
+    full: "aman mittal"
+  },
+  description: "self-taught nodejs developer",
+  location: "new delhi, India",
+  links: {
+    website: "http://amandeepmittal.github.io",
+    twitter: "https://twitter.com/amanhimself",
+    github: "https://github.com/amandeepmittal",
+    blog: "https://medium.com/@amanhimself",
+    medium: "https://medium.com/@amanhimself",
+    npm: "https://npmjs.com/~amandeepmittal",
+    atom: "https://atom.io/users/amandeepmittal"
+  },
+  primarySkills: ["nodejs", "expressjs", "mongodb", "angularjs"],
+  skills: {
+    serverSide: "node.js",
+    frameworks: "expressjs",
+    database: "mongodb",
+    clientSide: ["angularjs", "html", "javascript", "makrdown", "twitter bootstrap"],
+    templateEngines: ["jade/pug" ,"handlebars", "ejs"],
+    editors: ["atom", "webstorm"],
+    tools: ["git", "github", "npm", "mocha", "grunt", "yeoman"]
+  },
+  projects:{
+    trimify: {
+      description: "To remove lead and trail whitespace(s) from a JavaScript String [without using RegExp]",
+      link: "https://www.npmjs.com/package/trimify"
+    },
+    startrekNames: {
+      description: "Randomly Generate StarTrek characters",
+      link: "https://www.npmjs.com/package/startrek-names"
+    },
+    expressjs: {
+      description: "Expressjs, a Node.js framework, snippets package for Atom. Supports common ES6 syntax.",
+      link: "https://atom.io/packages/expressjs"
+    },
+    jade: {
+      description: "Jade/Pug snippets package for Atom.",
+      link: "https://atom.io/packages/jade"
+    },
+    atomNodejsSnippets: {
+      description: "Nodejs and Javascript snippets package for Atom. Supports common ES6 syntax",
+      link: "https://atom.io/packages/atom-nodejs-snippets"
+    }
+  },
+  articles: "https://medium.com/@amanhimself"
+};
